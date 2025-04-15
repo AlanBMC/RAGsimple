902c768f384b42e18981b014f25937f0897514c7
Alan Bruno Morais Costa <8761817@empresa.mti.mt.gov.br>
2025-04-15 11:42:02 -0400

    feat: agente mcp criado, realiza busca de locais bno airbnb - mcp teste

--- MCP/agenteSimples.py
+++ MCP/agenteSimples.py
@@ -1,13 +1,9 @@
 from praisonaiagents import Agent, MCP
 
 agent = Agent(
-    instructions="""You are a helpful assistant that can check stock prices and perform other tasks.
-    Use the available tools when relevant to answer user questions.""",
+    instructions="""You help book apartments on Airbnb.""",
     llm="ollama/gemma3:1b",
     tools = MCP("npx -y @openbnb/mcp-server-airbnb --ignore-robots-txt")
 )
 
-# NOTE: Python Path replace with yours: /Users/praison/miniconda3/envs/mcp/bin/python
-# NOTE: custom-python-server.py file path, replace it with yours: /Users/praison/stockprice/custom-python-server.py
-
-agent.start("What is the stock price of Tesla?")
+agent.start("I want to book an apartment in Paris for 2 nights. 04/28 - 04/30 for 2 adults")
